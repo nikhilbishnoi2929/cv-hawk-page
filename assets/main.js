84221288b0e818a66117b0aad62f43a4a2250a9b
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-09 14:33:05 -1200

    second section done

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,73 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu").classList.toggle("cross")
 }
+
+
+
+
+
+
+
+
+
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+  
+    arrows: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+   
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          autoplay: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+        }
+      },
+  
+  
+    ]
+  }); 
+  $('.responsive_2').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+  
+    arrows: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    
+    
+  }); 
