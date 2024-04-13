a7f58d52f6fa1f908baefcc8646309a61005be33
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-13 10:08:13 -1200

    add cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,34 @@ $('.responsive_3').slick({
 
   ]
 }); 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
+  });
+});
+ 
