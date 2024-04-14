a3af0d9a367e9292cf2f9831720c4b58e3a6e086
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-13 14:46:33 -1200

    bottom to top button fized

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@ $('.responsive_3').slick({
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
