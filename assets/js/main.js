678cae6b499738b9203a86e0457f50569b5533c0
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-04-18 12:11:06 -1200

    remove aos animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,7 @@ $('.responsive_3').slick({
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -150,8 +150,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-AOS.init(
-  {
-    duration: 1200,
-  }
-);
+ 
